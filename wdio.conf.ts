0db585fbfd71cffe22eba2757d40c15b65e298f4
Maulana Jayadi <maulanajayadi@Maulanas-MacBook-Air.local>
2024-09-12 10:33:31 +0700

    updateData

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -54,9 +54,7 @@ export const config: Options.Testrunner = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'chrome'
-    }],
+    services: ['chromedriver'],
 
     //
     // ===================
